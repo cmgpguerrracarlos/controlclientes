a6445badf65cad2d8e443b0cde125f03d8574fb5
cmgpBLapTop <cmgpguerracarlos@gmail.com>
2021-04-05 12:21:56 -0300

    get conexion to mongodb

--- src/main/java/uy/com/cmgp/datos/ConexionMongoDb.java
+++ src/main/java/uy/com/cmgp/datos/ConexionMongoDb.java
@@ -1,44 +1,50 @@
 package uy.com.cmgp.datos;
 import com.mongodb.*;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import java.util.ArrayList;
+import java.util.List;
+import org.bson.Document;
 
 public class ConexionMongoDb {
-    private DB database;
-    private DBCollection collection;
-    private BasicDBObject document = new BasicDBObject();
+    private MongoDatabase database;
+    private MongoCollection<Document> collection;
+    private Document document = new Document();
     
     public ConexionMongoDb(){
-        Mongo mongo = new Mongo("localhost",3456);
-        database = mongo.getDB("Todo");
-        collection = database.getCollection("Todo");
+        MongoClient mongoClient = new MongoClient("localhost",27017);
+         this.database = mongoClient.getDatabase("mydb");
+         this.collection = database.getCollection("clients");
         System.out.println("Conectado a mongodb");
         
     }
     
     public boolean insertar(String action){
-        document.put("action", action);
-        collection.insert(document);
+//        document.put("action", action);
+//        collection.insert(document);
         return true;
     }
     
     public void mostrar(){
-        DBCursor cursor = collection.find();
-        while(cursor.hasNext()){
-            System.out.println(cursor.next());
-        }
+        List<Document> databases = new MongoClient("localhost",27017).listDatabases().into(new ArrayList<>());
+
+            databases.forEach(db -> System.out.println(db.toJson()));
     }
     
     public boolean actualizar(String oldAction,String newAction){
-        document.put("action", oldAction);
-        BasicDBObject newDocument = new BasicDBObject();
-        newDocument.put("action", newAction);
-        collection.findAndModify(document, newDocument);
-        System.out.println("Actualizado");
+//        document.put("action", oldAction);
+//        BasicDBObject newDocument = new BasicDBObject();
+//        newDocument.put("action", newAction);
+//        collection.findAndModify(document, newDocument);
+//        System.out.println("Actualizado");
         return true;
     }
     
     public boolean eliminar(String action){
-        document.put("action", action);
-        collection.remove(document);
+//        document.put("action", action);
+//        collection.remove(document);
         return true;
     }
+    
+    
 }
